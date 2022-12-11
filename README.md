ff207efbb2733d4aad3a1d3e993d41f5a3f74928
Mike Nielsen <mmni@protonmail.com>
2022-12-12 00:15:45 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Native runtime
+* Install graalvm
+* Set java home to graalvm
+* Install VS 2019 build tools
+* Setup environment with "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
+* Run mvn -Pnative native:compile
+
+Location of VS Studio download:
+
+https://visualstudio.microsoft.com/thank-you-downloading-visual-studio/?sku=Community&channel=Release&version=VS2022&source=VSLandingPage&cid=2030&passive=false
---
+++ src/main/java/com/example/springbootthree/SpringbootThreeApplication.java
@@ -0,0 +1,25 @@
+package com.example.springbootthree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SpringbootThreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootThreeApplication.class, args);
+    }
+
+}
+@RestController
+@RequestMapping("/")
+class SpaceRestController{
+
+    @GetMapping("")
+    public String hello(){
+        return "Hello astronauts...";
+    }
+}
